0a434cf2db1fb9c62731d8b2f5f46a2f69f70093
012faressaad778-lang <012faressaad778@gmail.com>
2026-06-27 00:01:53 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/chat", async (req, res) => {
+
+const message = req.body.message;
+
+res.json({
+reply:"أنت قلت: " + message
+});
+
+});
+
+app.listen(3000,()=>{
+console.log("Fares AI Running...");
+});
